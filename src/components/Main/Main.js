bcd2c372ffd127ccda540e6bd41bb62f86d2f768
Stanislav <infleshlightwetrust@gmail.com>
2022-01-11 12:05:06 +0300

    feature/finish-social-component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,15 +4,23 @@ import dsBanner from "../../images/ds1-banner.jpg";
 import Advertisement from "./Advertisement/Advertisement";
 import About from "./About/About";
 import Social from "./Social/Social";
+import classNames from "classnames";
 
 
 function Main(props) {
+    const mainBannerClass = classNames(
+        "main__banner",
+        {
+            "main__banner_disabled": props.pageYOffset > 450,
+        }
+    )
+
     return (
         <main className="main" >
             <Advertisement />
-            <img className="main__banner" src={dsBanner} alt="Баннер игры Дарк Соулс"/>
+            <img className={mainBannerClass} src={dsBanner} alt="Баннер игры Дарк Соулс"/>
             <About pageYOffset={props.pageYOffset}/>
-            <Social />
+            <Social pageYOffset={props.pageYOffset}/>
         </main>
     )
 };
