8418db9b15cf5227babb319c467f0ac94f06841a
Stanislav <infleshlightwetrust@gmail.com>
2022-01-14 11:42:55 +0300

    feature/create-footer-component

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import "./App.less";
 import Popup from "./Popup/Popup";
 import Header from "./Header/Header";
 import Main from "./Main/Main";
+import Footer from "./Footer/Footer"
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <Popup />
+      {/* <Popup /> */}
       <Main />
+      <Footer />
     </div>
   );
 };
